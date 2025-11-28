11b07371301920731cad6786a07758c593758aad
LittleAngel4ever <Minsk_x250@tutamail.com>
2025-11-28 09:18:30 +0300

    4

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/script.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: 'script.js',
   },
 };
