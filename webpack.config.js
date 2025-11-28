3761ce2c877ebe34a0c3d800847dbb9708e366d1
LittleAngel4ever <Minsk_x250@tutamail.com>
2025-11-28 09:42:39 +0300

    6

--- webpack.config.js
+++ webpack.config.js
@@ -11,5 +11,13 @@ module.exports = {
     new HtmlWebpackPlugin({
         template: "./src/index.html"
     })
-  ]
+  ],
+   module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+    ],
+  },
 };
